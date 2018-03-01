576b7371ba9b3271458089bafd1b89b49bb37819
DamianoBarbiero <damiano_junior@hotmail.com>
2018-03-01 20:54:51 -0300

    implantacao

---
+++ Aula2RMIComum/src/comum/Professor.java
@@ -0,0 +1,17 @@
+package comum;
+
+import java.rmi.RemoteException;
+
+public class Professor extends Pessoa{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -329964420439750625L;
+
+	public Professor() throws RemoteException{
+		super();
+	}
+	
+
+}
---
+++ Aula2RMIComum/src/comum/ServicoRemotoImpl.java
@@ -0,0 +1,29 @@
+package comum;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ServicoRemotoImpl extends UnicastRemoteObject implements IServicoRemoto{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2293791170403550531L;
+	private static List<Pessoa> lstPessoa = new ArrayList<>();
+	
+	public ServicoRemotoImpl() throws RemoteException{
+	}
+
+	@Override
+	public void inserir(Pessoa p) throws RemoteException{
+		lstPessoa.add(p);
+	}
+	
+	@Override
+	public List<Pessoa> listarPessoa() throws RemoteException{
+		return lstPessoa;
+	}
+}
